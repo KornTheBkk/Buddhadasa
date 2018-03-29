7db76772a73014eb2a230eb2de41754ecac66314
kornNYPD <kornthebkk@gmail.com>
2018-03-30 05:03:09 +0700

    Initialize project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { SoundPageModule } from '../pages/sound/sound.module';
+import { BookPageModule } from '../pages/book/book.module';
+import { SettingPageModule } from '../pages/setting/setting.module';
+
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -14,20 +16,19 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    SoundPageModule,
+    BookPageModule,
+    SettingPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
     TabsPage
   ],
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
+import { SoundPage } from '../sound/sound';
+import { BookPage } from '../book/book';
+import { SettingPage } from '../setting/setting';
+
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tabHome = HomePage;
+  tabSound = SoundPage;
+  tabBook = BookPage;
+  tabSetting = SettingPage;
 
   constructor() {
 
