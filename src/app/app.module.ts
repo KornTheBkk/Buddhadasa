3256ec9a5fab778cf6cd1490f83f43aef8aadaa9
kornNYPD <kornthebkk@gmail.com>
2018-04-12 05:04:38 +0700

    add android platform

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -75,7 +75,7 @@ import { SearchProvider } from '../providers/search/search';
     SoundProvider,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
     //{ provide: 'API_URL', useValue: 'http://localhost/buddha/services/api' },
-    { provide: 'API_URL', useValue: 'http://172.20.10.2/buddhadasa/api' },
+    { provide: 'API_URL', useValue: 'http://172.20.10.3/buddhadasa/api' },
     SearchProvider
   ]
 })
