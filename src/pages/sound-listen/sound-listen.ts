039b92967e9b8af599e25d62ff44f2c97796c7fa
kornNYPD <kornthebkk@gmail.com>
2018-04-16 04:00:47 +0700

    SongListenPage : download sound to device local with file transfer

--- src/pages/sound-listen/sound-listen.ts
+++ src/pages/sound-listen/sound-listen.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams, Platform } from 'ionic-angular';
+import { Component, NgZone } from '@angular/core';
+import { NavController, NavParams, Platform, LoadingController, Loading } from 'ionic-angular';
 
 import { Media, MediaObject } from '@ionic-native/media';
 import { BackgroundMode } from '@ionic-native/background-mode';
+import { FileTransfer, FileTransferObject } from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
 
 import { Observable } from 'rxjs/rx';
 import { Subscription } from "rxjs/Subscription";
@@ -18,45 +20,67 @@ export class SoundListenPage {
 
   sound: ISound;
 
-  file: MediaObject;
+  mediaObject: MediaObject;
+  loadedProgress: number = 0;
+  soundReady: boolean = false; // control by download 
   audio: any;
   isLoading: boolean = false;
 
-  filename: string;
+  loader: Loading;
+
   isPlaying: boolean = false;
-  duration: number = 0;
+  duration: number = -1;
   currentPosition: number = -1;
-  tempPosition: number = 1; // สำหรับเก็บ currentPosition ไว้เพื่อเช็ค position ซ้ำ ถ้าซ้ำแสดงว่ากำหลังโหลดอยู่ จะให้ขึ้น loading
-  obsPosition: Subscription;
+  subsPosition: Subscription;
   seekPosition: number;
 
   skipStep: number = 60;
-  messageAlert: string = null;
+
+  storagePath: string; // path for store file to local device
+  fileUrl: string; // source file
+  fileName: string; // file name on target
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public platform: Platform,
     private media: Media,
-    private backgroundMode: BackgroundMode) {
+    private backgroundMode: BackgroundMode,
+    private transfer: FileTransfer,
+    private file: File,
+    private loadingCtrl: LoadingController,
+    private _zone: NgZone) {
 
-    
+    platform.ready().then(() => {
+
+      this.backgroundMode.enable();
+
+      if (this.platform.is('ios')) {
+        this.storagePath = this.file.documentsDirectory;
+      } else {
+        this.storagePath = this.file.dataDirectory;
+      }
+
+      this.storagePath = this.storagePath + 'buddhadasa/sound/';
+    });
 
     this.audio = navParams.data;
-    this.filename = this.audio.mp3_file;
-    //console.log(this.audio.mp3_file);
-    //console.log('audio: ' + JSON.stringify(this.audio));
+
+    this.fileUrl = this.audio.mp3_file; // source file
+    this.fileName = this.audio.id + '.mp3'; // destination file name
+
+    this.loader = this.loadingCtrl.create();   
   }
 
-  ionViewWillEnter(){
+  ionViewWillEnter() {
     this.platform.ready().then(() => {
-      this.initializeMedia();
+      this.downloadSound();
     });
   }
 
   ionViewWillLeave() {
-    this.file.release();
-    this.stop();
+    this.mediaObject.release();
+    //this.stop();
   }
 
   search() {
@@ -65,128 +89,23 @@ export class SoundListenPage {
 
   initializeMedia() {
 
-    this.backgroundMode.enable();
-    this.file = this.media.create(this.filename);
-    this.play(); // set autoplay
 
-    this.file.onError.subscribe(error => console.log('Error!', JSON.stringify(error)));
-
-    this.file.onStatusUpdate.subscribe(status => {
-      //console.log('on status update: ' + status);
-      //console.log('init duration: ' + this.duration);
-    });
+    //this.mediaObject.onError.subscribe(error => console.log('Error!', JSON.stringify(error)))
+  
   }
 
   changeSeekTo() {
     if (this.seekPosition != this.currentPosition && this.currentPosition > 0) {
-      this.file.seekTo(this.seekPosition * 1000);
+      this.mediaObject.seekTo(this.seekPosition * 1000);
       //console.log('seek to: ' + this.seekPosition);
       this.currentPosition = this.seekPosition;
     }
   }
 
-  play() {
-
-    if (this.isPlaying) {
-      this.pause();
-
-    } else {
-
-      if (!this.backgroundMode.isEnabled()) {
-        this.backgroundMode.enable();
-      }
-
-      this.isLoading = true
-      //this.messageAlert = "กำลังโหลดเสียง..."
-
-      this.isPlaying = true;
-      this.file.play();
-
-      this.obsPosition = Observable.interval(1000).subscribe(() => {
-
-        this.file.getCurrentPosition().then((position) => {
-
-          position = Math.floor(position);
-
-          //console.log(`isLoading: ${this.isLoading}, position: ${position}, tempPosition: ${this.tempPosition}`);
-
-          if (position >= 1) {
-            if (this.tempPosition == 1) { //ทำครั้งแรกครั้งเดียว
-              this.isLoading = false;
-            }
-            //เช็คเมื่อ playing อยู่แล้วมีการโหลดจากเน็ต
-            if (position >= 2 && (position == this.tempPosition) && !this.isLoading) {
-              //console.log('loading present');
-              this.isLoading = true;
-              //this.messageAlert = "กำลังโหลดเสียง..."
-
-            } else if (this.isLoading && position != this.tempPosition) {
-              //console.log('loading dismiss');
-              this.isLoading = false;
-              this.messageAlert = null;
-            }
-          }
-
-          if (this.isLoading) {
-            //console.log('refresh player');
-
-            //refresh player: pause & play : ถ้าไม่ทำแบบนี้ เน็ตช้าๆ มันจะค้างและไม่เล่นต่อ
-            this.refreshPlayer();
-          }
-
-          this.tempPosition = position;
-
-          if (position >= 1 && (position == this.duration)) {
-            this.currentPosition = 0;
-            this.seekPosition = 0;
-            this.isPlaying = false;
-            this.isLoading = false;
-            this.file.stop();
-            this.obsPosition.unsubscribe();
-            //this.file.play();
-
-            console.log('stop then loop');
-
-            //this.messageAlert = "กำลังโหลดเสียง..."
-            //this.stop(); // ถ้าเอา comment ออก จะไม่เล่นต่อเนื่องเมื่อจบ
-          } else {
-            this.currentPosition = position;
-            this.seekPosition = position;
-          }
-
-          if (this.duration <= 0) { // ทำครั้งแรกเท่านั้น หรือดึงข้อมูลมาแล้วอาจจะได้ -1
-            this.duration = Math.floor(this.file.getDuration());
-          }
-
-
-          //console.log('current position: ' + this.currentPosition);
-        });
-
-      });
-    }
-  }
-
-  refreshPlayer() {
-    if (this.isPlaying) {
-      this.file.pause();
-      this.file.play();
-    }
-
-  }
-
-  pause() {
-    if (this.isPlaying) {
-      this.obsPosition.unsubscribe();
-      this.isPlaying = false;
-      this.file.pause();
-      //console.log('playing: ' + this.playing);
-    }
-  }
-
   stop() {
-    this.obsPosition.unsubscribe();
+    this.subsPosition.unsubscribe();
     // this.backgroundMode.disable();
-    this.file.stop();
+    this.mediaObject.stop();
     this.currentPosition = 0;
     this.seekPosition = 0;
     this.isPlaying = false;
@@ -200,7 +119,7 @@ export class SoundListenPage {
       seekTo = 0;
       this.seekPosition = 0;
     }
-    this.file.seekTo(seekTo * 1000);
+    this.mediaObject.seekTo(seekTo * 1000);
   }
 
   skipBackward() {
@@ -209,8 +128,110 @@ export class SoundListenPage {
       seekTo = 0;
       this.seekPosition = 0;
     }
-    this.file.seekTo(seekTo * 1000);
+    this.mediaObject.seekTo(seekTo * 1000);
   }
 
+
+  downloadSound() {
+
+    this.platform.ready().then(() => {
+
+      this.loader.present();
+      this.loader.setContent(`ดาวน์โหลดเสียง ${this.loadedProgress}%`);
+
+      let filePath: string;
+
+      filePath = this.storagePath + this.fileName;
+
+      // this.file.checkFile(this.storagePath, this.fileName)
+      //   .then(() => {
+
+      //     console.log('file found');
+
+      //   })
+      //   .catch(error => {
+
+      //console.log('file not found : ' + filePath);
+
+      let transfer: FileTransferObject = this.transfer.create();
+
+      transfer.download(this.fileUrl, filePath)
+        .then(entry => {
+
+          let url = entry.toURL();
+          this.soundReady = true;
+
+          let fileName = this.storagePath + this.fileName;
+          console.log('fileName: ' + fileName);
+          this.mediaObject = this.media.create(fileName.replace(/^file:\/\//, ''));
+          this.playSound();
+
+          console.log('download success : ' + url);
+
+        })
+        .catch(error => {
+
+          console.log('download failed : ' + JSON.stringify(error));
+
+        });
+
+      transfer.onProgress((progressEvent) => {
+        //console.log('on progress: ' + JSON.stringify(progressEvent));
+        //this.loadedProgress = Math.round(progressEvent.loaded / progressEvent.total);
+        this._zone.run(() => {
+          this.loadedProgress = (progressEvent.lengthComputable) ? Math.floor(progressEvent.loaded / progressEvent.total * 100) : -1;
+          console.log('loadedProgress: ' + this.loadedProgress + '%');
+          this.loader.setContent(`ดาวน์โหลดเสียง ${this.loadedProgress}%`);
+
+          if (this.loadedProgress == 100) {
+            this.loader.dismiss();
+          }
+        });
+      });
+
+    });
+
+    //});
+  }
+
+  playSound() {
+
+    if (this.soundReady) {
+
+      if (!this.isPlaying) {
+
+        this.isPlaying = true;
+
+        this.mediaObject.play();
+
+        this.subsPosition = Observable.interval(1000).subscribe(() => {
+
+          this.mediaObject.getCurrentPosition().then((position) => {
+
+            this.currentPosition = Math.floor(position);
+            //console.log('current position : ' + this.currentPosition);
+            
+            if (this.duration == -1) {
+              this.duration = Math.floor(this.mediaObject.getDuration());
+            }
+
+            if (this.currentPosition >= 1 && (this.currentPosition == this.duration)) {
+              this.subsPosition.unsubscribe();
+              this.currentPosition = 0;
+              this.isPlaying = false;
+            }
+          });
+
+        });
+
+      } else {
+
+        this.isPlaying = false;
+
+        this.mediaObject.pause();
+        this.subsPosition.unsubscribe();
+      }
+    }
+  }
 
 }
