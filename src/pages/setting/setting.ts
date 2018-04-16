d46201a93569c3ee2f298e26884266419dea2586
kornNYPD <kornthebkk@gmail.com>
2018-04-16 15:25:11 +0700

    SettingPage : add removeDir when book/sound downloaded = false

--- src/pages/setting/setting.ts
+++ src/pages/setting/setting.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, NavParams, Platform } from 'ionic-angular';
 import { SearchPage } from '../search/search';
 
+import { File } from '@ionic-native/file';
+
 @Component({
   selector: 'page-setting',
   templateUrl: 'setting.html',
@@ -12,10 +14,13 @@ export class SettingPage {
   bookDownloaded: boolean;
   soundDownloaded: boolean;
 
+  path: string;
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public platform: Platform) {
+    private platform: Platform,
+    private file: File) {
 
     this.platform.ready().then(() => {
 
@@ -23,11 +28,18 @@ export class SettingPage {
       this.bookDownloaded = JSON.parse(localStorage.getItem('bookDownloaded'));
       this.soundDownloaded = JSON.parse(localStorage.getItem('soundDownloaded'));
 
+      if (this.platform.is('ios')) {
+        this.path = this.file.documentsDirectory;
+      } else {
+        this.path = this.file.dataDirectory;
+      }
+
+      this.path = this.path + '/buddhadasa';
+
     });
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SettingPage');
   }
 
   search() {
@@ -38,11 +50,32 @@ export class SettingPage {
 
     if (name == 'push') {
       localStorage.setItem('pushNotification', this.pushNotification.toString());
+
     } else if (name == 'book') {
       localStorage.setItem('bookDownloaded', this.bookDownloaded.toString());
+
+      if (!this.bookDownloaded) {
+        this.removeDir('book');
+      }
+
     } else if (name == 'sound') {
       localStorage.setItem('soundDownloaded', this.soundDownloaded.toString());
+
+      if (!this.soundDownloaded) {
+        this.removeDir('sound');
+      }
     }
+  }
+
+  removeDir(dirName: string) {
+
+    this.file.removeRecursively(this.path, dirName)
+      .then(removeResult => {
+        console.log('removeDir: ' + JSON.stringify(removeResult));
+      })
+      .catch(error => {
+        console.log('removeDir error: ' + JSON.stringify(error));
+      });
   }
 
 
